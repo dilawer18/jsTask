fb5b06b43add5a01cf5255792f0e789fa44078e0
dilawer18 <dilawer1807948@gmail.com>
2022-10-07 12:58:50 +0530

    add new file table.j

---
+++ table.js
@@ -0,0 +1,14 @@
+
+ 
+   function mulTable(number){
+    for(let index=1;index<=10;index++){
+        
+        let newTable= number*index
+        
+        console.log(`${number} * ${index} = ${newTable}`);
+   }
+   }
+   mulTable(3);
+    
+    
+
